042e474aac4f82397933482de90d9db63f92802c
Gurpreet <gurpreetsinghghatora1@gmail.com>
2024-04-01 00:18:34 -0400

    Favourite Actors
    This project is a website showcasing information about favorite actors. It's built using Express.js and EJS templates, featuring individual pages for each actor. Users can explore details about the actor's career, notable performances, and personal background. The site includes a shared header with navigation links for easy browsing. Routing is configured in the routes/index.js file to load the specific actor's page when selected. Git version control is utilized for effective collaboration and project history management.

--- LAB04/my-project/routes/index.js
+++ LAB04/my-project/routes/index.js
@@ -1,26 +1,25 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-/* GET actor1 page. */
 router.get('/actor1', function(req, res, next) {
   res.render('actor1');
 });
-/* GET actor1 page. */
+
 router.get('/actor2', function(req, res, next) {
   res.render('actor2');
 });
-/* GET actor1 page. */
+
 router.get('/actor3', function(req, res, next) {
   res.render('actor3');
 });
-/* GET actor1 page. */
+
 router.get('/actor4', function(req, res, next) {
   res.render('actor4');
 });
 
 module.exports = router;
+
